OfirTabach/amzn-drivers

commit e011adb134a9acbb200f67e881f9e7cde8a9def0

linux/ena: Unify handle_invalid_req_id definitions
The handling for invalid request id in TX completions is the same except
for the string that is printed in case of an error.
This patch creates a single function which deduces by itself what kind
of traffic goes through the TX queue.

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -968,37 +968,49 @@ void ena_get_and_dump_head_tx_cdesc(struct ena_com_io_cq *io_cq)
 }
 
 int handle_invalid_req_id(struct ena_ring *ring, u16 req_id,
-			  struct ena_tx_buffer *tx_info, bool is_xdp)
+			  struct ena_tx_buffer *tx_info)
 {
-	if (tx_info)
-		netif_err(ring->adapter,
-			  tx_done,
-			  ring->netdev,
-			  "tx_info doesn't have valid %s. qid %u req_id %u",
-			   is_xdp ? "xdp frame" : "skb", ring->qid, req_id);
+	struct ena_adapter *adapter = ring->adapter;
+	char *queue_type;
+
+	if (ENA_IS_XDP_INDEX(adapter, ring->qid))
+		queue_type = "XDP";
+#ifdef ENA_AF_XDP_SUPPORT
+	else if (ENA_IS_XSK_RING(ring))
+		queue_type = "ZC queue";
+#endif
 	else
-		netif_err(ring->adapter,
+		queue_type = "regular";
+
+	if (tx_info)
+		netif_err(adapter,
 			  tx_done,
 			  ring->netdev,
-			  "Invalid req_id %u in qid %u\n",
-			  req_id, ring->qid);
+			  "req id %u doesn't correspond to a packet. qid %u queue type: %s",
+			   ring->qid, req_id, queue_type);
+	else
+		netif_err(adapter,
+			  tx_done,
+			  ring->netdev,
+			  "Invalid req_id %u in qid %u, queue type: %s\n",
+			  req_id, ring->qid, queue_type);
 
 	ena_get_and_dump_head_tx_cdesc(ring->ena_com_io_cq);
 	ena_increase_stat(&ring->tx_stats.bad_req_id, 1, &ring->syncp);
-	ena_reset_device(ring->adapter, ENA_REGS_RESET_INV_TX_REQ_ID);
+	ena_reset_device(adapter, ENA_REGS_RESET_INV_TX_REQ_ID);
 
 	return -EFAULT;
 }
 
-static int validate_tx_req_id(struct ena_ring *tx_ring, u16 req_id)
+int validate_tx_req_id(struct ena_ring *tx_ring, u16 req_id)
 {
 	struct ena_tx_buffer *tx_info;
 
 	tx_info = &tx_ring->tx_buffer_info[req_id];
-	if (likely(tx_info->skb))
+	if (likely(tx_info->total_tx_size))
 		return 0;
 
-	return handle_invalid_req_id(tx_ring, req_id, tx_info, false);
+	return handle_invalid_req_id(tx_ring, req_id, tx_info);
 }
 
 static int ena_clean_tx_irq(struct ena_ring *tx_ring, u32 budget)

kernel/linux/ena/ena_netdev.h
@@ -662,7 +662,8 @@ int ena_restore_device(struct ena_adapter *adapter);
 void ena_get_and_dump_head_tx_cdesc(struct ena_com_io_cq *io_cq);
 void ena_get_and_dump_head_rx_cdesc(struct ena_com_io_cq *io_cq);
 int handle_invalid_req_id(struct ena_ring *ring, u16 req_id,
-			  struct ena_tx_buffer *tx_info, bool is_xdp);
+			  struct ena_tx_buffer *tx_info);
+int validate_tx_req_id(struct ena_ring *tx_ring, u16 req_id);
 
 static inline void ena_ring_tx_doorbell(struct ena_ring *tx_ring)
 {
@@ -695,7 +696,7 @@ int ena_refill_rx_bufs(struct ena_ring *rx_ring, u32 num);
 static inline void handle_tx_comp_poll_error(struct ena_ring *tx_ring, u16 req_id, int rc)
 {
 	if (unlikely(rc == -EINVAL))
-		handle_invalid_req_id(tx_ring, req_id, NULL, true);
+		handle_invalid_req_id(tx_ring, req_id, NULL);
 	else if (unlikely(rc == -EFAULT)) {
 		ena_get_and_dump_head_tx_cdesc(tx_ring->ena_com_io_cq);
 		ena_reset_device(tx_ring->adapter,

kernel/linux/ena/ena_xdp.c
@@ -6,21 +6,6 @@
 #include "ena_xdp.h"
 #ifdef ENA_XDP_SUPPORT
 
-static int validate_xdp_req_id(struct ena_ring *tx_ring, u16 req_id)
-{
-	struct ena_tx_buffer *tx_info;
-
-	tx_info = &tx_ring->tx_buffer_info[req_id];
-#ifdef ENA_AF_XDP_SUPPORT
-	if (likely(tx_info->total_tx_size))
-#else
-	if (likely(tx_info->xdpf))
-#endif
-		return 0;
-
-	return handle_invalid_req_id(tx_ring, req_id, tx_info, true);
-}
-
 static int ena_xdp_tx_map_frame(struct ena_ring *tx_ring,
 				struct ena_tx_buffer *tx_info,
 				struct xdp_frame *xdpf,
@@ -557,7 +542,7 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 		}
 
 		/* validate that the request id points to a valid xdp_frame */
-		rc = validate_xdp_req_id(tx_ring, req_id);
+		rc = validate_tx_req_id(tx_ring, req_id);
 		if (rc)
 			break;
 

commit 1a44eb028f233652f58549265ccd22978fcd4246

linux/ena: Fix print mismatch between qid and req id
Fixing the order of parameters in the TX non matching
request ID error print.

Fixes: e011adb134a9 ("linux/ena: Unify handle_invalid_req_id definitions")
Signed-off-by: David Arinzon <darinzon@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -985,7 +985,7 @@ int handle_invalid_req_id(struct ena_ring *ring, u16 req_id,
 			  tx_err,
 			  ring->netdev,
 			  "req id %u doesn't correspond to a packet. qid %u queue type: %s",
-			   ring->qid, req_id, queue_type);
+			  req_id, ring->qid, queue_type);
 	else
 		netif_err(adapter,
 			  tx_err,